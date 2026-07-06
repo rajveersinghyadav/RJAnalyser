f2dc22786d1b0639a2e2638a7d9cef90303acbd7
rajveersinghyadav <storynovausaofficial@gmail.com>
2026-07-06 11:27:35 +0530

    Update brain.js

--- ai/brain.js
+++ ai/brain.js
@@ -1,1 +1,61 @@
+/*
+=========================================
+RJAnalyser AI Brain V1
+Created By: Rajveer + ChatGPT
+=========================================
+*/
 
+const RJBrain = {
+
+    version: "1.0",
+
+    status: "ONLINE",
+
+    learning: true,
+
+    memory: [],
+
+    analyse(chart){
+
+        return {
+
+            chart: chart,
+
+            trend: "Scanning...",
+
+            pattern: "Searching...",
+
+            confidence: "0%",
+
+            recommendation: "Waiting for AI Engine"
+
+        };
+
+    },
+
+    learn(rule){
+
+        this.memory.push(rule);
+
+        localStorage.setItem(
+            "RJ_AI_MEMORY",
+            JSON.stringify(this.memory)
+        );
+
+    },
+
+    load(){
+
+        let data=localStorage.getItem("RJ_AI_MEMORY");
+
+        if(data){
+
+            this.memory=JSON.parse(data);
+
+        }
+
+    }
+
+};
+
+RJBrain.load();
