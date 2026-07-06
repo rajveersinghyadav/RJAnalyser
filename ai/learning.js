58bde01431567e7af0c36b96622a535282e85201
rajveersinghyadav <storynovausaofficial@gmail.com>
2026-07-06 11:28:14 +0530

    Update learning.js

--- ai/learning.js
+++ ai/learning.js
@@ -1,1 +1,56 @@
+/*
+=========================================
+RJAnalyser Learning Engine V1
+=========================================
+*/
 
+const RJLearning = {
+
+    rules: [],
+
+    teach(title, description) {
+
+        const rule = {
+            id: Date.now(),
+            title: title,
+            description: description,
+            created: new Date().toLocaleString()
+        };
+
+        this.rules.push(rule);
+        this.save();
+
+        return "Learning Saved Successfully";
+
+    },
+
+    save() {
+
+        localStorage.setItem(
+            "RJ_LEARNING_DATABASE",
+            JSON.stringify(this.rules)
+        );
+
+    },
+
+    load() {
+
+        let data = localStorage.getItem("RJ_LEARNING_DATABASE");
+
+        if (data) {
+
+            this.rules = JSON.parse(data);
+
+        }
+
+    },
+
+    getAll() {
+
+        return this.rules;
+
+    }
+
+};
+
+RJLearning.load();
