9cab0662f6553139ca0747a358f69f412f6ad587
rajveersinghyadav <storynovausaofficial@gmail.com>
2026-07-06 19:01:50 +0530

    Update engine.js

--- ai/engine.js
+++ ai/engine.js
@@ -1,1 +1,51 @@
+/*
+=========================================
+RJAnalyser Engine V1
+Main AI Workflow
+=========================================
+*/
 
+const RJEngine = {
+
+    version: "1.0",
+
+    analyse(input) {
+
+        console.log("===== RJAnalyser Started =====");
+
+        // Brain
+        RJBrain.start();
+
+        // Memory
+        RJMemory.add("analysis", input);
+
+        // Pattern
+        let pattern = RJPattern.find("Bullish Engulfing");
+
+        // Reasoning
+        let report = RJReasoning.analyse({
+
+            trend: "Bullish",
+
+            pattern: pattern ? pattern.name : ""
+
+        });
+
+        // Decision
+        let decision = RJDecision.decide(report);
+
+        return {
+
+            input,
+
+            pattern,
+
+            report,
+
+            decision
+
+        };
+
+    }
+
+};
