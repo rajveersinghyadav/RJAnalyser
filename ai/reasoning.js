1caeb94006cdbf4200b1758b1ad9cb5521ae735e
rajveersinghyadav <storynovausaofficial@gmail.com>
2026-07-06 13:22:49 +0530

    Create reasoning.js

---
+++ ai/reasoning.js
@@ -0,0 +1,48 @@
+/*
+=========================================
+RJAnalyser Reasoning Engine V1
+=========================================
+*/
+
+const RJReasoning = {
+
+    analyse(data) {
+
+        let report = {
+
+            trend: "Unknown",
+
+            confidence: 0,
+
+            decision: "WAIT",
+
+            reason: []
+
+        };
+
+        if (data.trend === "Bullish") {
+
+            report.trend = "Bullish";
+            report.confidence += 30;
+            report.reason.push("Market trend is bullish.");
+
+        }
+
+        if (data.pattern === "Bullish Engulfing") {
+
+            report.confidence += 40;
+            report.reason.push("Bullish Engulfing detected.");
+
+        }
+
+        if (report.confidence >= 70) {
+
+            report.decision = "BUY";
+
+        }
+
+        return report;
+
+    }
+
+};
