06d2bb30f29d3c7a4f593856bd2d9eb910cf84a4
rajveersinghyadav <storynovausaofficial@gmail.com>
2026-07-09 09:19:04 +0530

    Update assets.js

--- market/assets.js
+++ market/assets.js
@@ -1,1 +1,57 @@
+const RJAssets = {
 
+forex: [
+
+"EURUSD",
+
+"GBPUSD",
+
+"USDJPY",
+
+"AUDUSD",
+
+"USDCAD",
+
+"USDCHF",
+
+"EURJPY",
+
+"GBPJPY"
+
+],
+
+crypto: [
+
+"BTCUSDT",
+
+"ETHUSDT",
+
+"SOLUSDT",
+
+"BNBUSDT",
+
+"XRPUSDT",
+
+"DOGEUSDT"
+
+],
+
+commodities: [
+
+"XAUUSD",
+
+"XAGUSD"
+
+],
+
+indices: [
+
+"NAS100",
+
+"US30",
+
+"SPX500"
+
+]
+
+};
