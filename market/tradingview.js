b36b1af5e6ea3a75fb8afe3a81ed0cb17dfa4019
rajveersinghyadav <storynovausaofficial@gmail.com>
2026-07-09 09:26:51 +0530

    Update tradingview.js

--- market/tradingview.js
+++ market/tradingview.js
@@ -1,1 +1,33 @@
+function loadTradingView(symbol = "BINANCE:BTCUSDT") {
 
+document.getElementById("tradingview_chart").innerHTML = "";
+
+new TradingView.widget({
+
+"autosize": true,
+
+"symbol": symbol,
+
+"interval": "15",
+
+"timezone": "Etc/UTC",
+
+"theme": "dark",
+
+"style": "1",
+
+"locale": "en",
+
+"toolbar_bg": "#161b22",
+
+"enable_publishing": false,
+
+"hide_top_toolbar": false,
+
+"allow_symbol_change": false,
+
+"container_id": "tradingview_chart"
+
+});
+
+}
